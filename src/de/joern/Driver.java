07f0992fd8db923cac7b0847353ebece55d983bd
Jörn Schumacher <joernspam@gmx.de>
2021-12-14 14:12:53 +0100

    refactored problems access in driver and test

--- src/de/joern/Driver.java
+++ src/de/joern/Driver.java
@@ -1,48 +1,25 @@
 package de.joern;
 
-import de.joern.day10.Day10;
-import de.joern.day1.Day1_1;
-import de.joern.day1.Day1_2;
-import de.joern.day11.Day11;
-import de.joern.day12.Day12;
-import de.joern.day13.Day13;
-import de.joern.day14.Day14;
-import de.joern.day2.Day2_1;
-import de.joern.day2.Day2_2;
-import de.joern.day3.Day3_1;
-import de.joern.day3.Day3_2;
-import de.joern.day4.Day4_1;
-import de.joern.day4.Day4_2;
-import de.joern.day5.Day5;
-import de.joern.day6.Day6;
-import de.joern.day7.Day7;
-import de.joern.day8.Day8_1;
-import de.joern.day8.Day8_2;
-import de.joern.day9.Day9_1;
-import de.joern.day9.Day9_2;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Arrays;
 import java.util.List;
-import java.util.function.Supplier;
-import java.util.stream.Collectors;
 
 public class Driver {
 
     public static void main(String[] args) {
-        for (PROBLEMS p : PROBLEMS.values()) {
+        for (Problems p : Problems.values()) {
             execute(p);
         }
     }
 
-    public static void execute(PROBLEMS problem) {
+    public static void execute(Problems problem) {
         try {
             List<ProblemSolver> solvers = problem.getSolvers();
+            var input = getFile(problem);
             for (ProblemSolver solver : solvers) {
-                Files.lines(problem.getFile()).forEach(solver::consider);
+                Files.lines(input).forEach(solver::consider);
                 solver.finished();
             }
         } catch (IOException e) {
@@ -50,42 +27,9 @@ public class Driver {
         }
     }
 
-    enum PROBLEMS {
-        DAY1(1, Day1_1::new, Day1_2::new),
-        DAY2(2, Day2_1::new, Day2_2::new),
-        DAY3(3, Day3_1::new, Day3_2::new),
-        DAY4(4, Day4_1::new, Day4_2::new),
-        DAY5(5, Day5::day5_1, Day5::day5_2),
-        DAY6(6, Day6::day6_1, Day6::day6_2),
-        DAY7(7, Day7::day7_1, Day7::day7_2),
-        DAY8(8, Day8_1::new, Day8_2::new),
-        DAY9(9, Day9_1::new, Day9_2::new),
-        DAY10(10, Day10::day10_1),
-        DAY11(11, Day11::day11_1),
-        DAY12(12, Day12::day12_1, Day12::day12_2),
-        DAY13(13, Day13::new),
-        DAY14(14, Day14::day14_1, Day14::day14_2),
-        ;
-
-        public final int day;
-        private final List<Supplier<ProblemSolver>> solver;
-
-        @SafeVarargs
-        PROBLEMS(int day, Supplier<ProblemSolver>... solver) {
-            this.day = day;
-            this.solver = Arrays.asList(solver);
-        }
-
-        public Path getFile() {
-            String filename = String.format("day%d.txt", day);
-            return Paths.get("data", filename);
-        }
-
-        public List<ProblemSolver> getSolvers() {
-            return solver.stream()
-                    .map(Supplier::get)
-                    .collect(Collectors.toList());
-        }
+    public static Path getFile(Problems problem) {
+        String filename = String.format("day%d.txt", problem.getDay());
+        return Paths.get("data", filename);
     }
 }
 
--- src/de/joern/test/Test.java
+++ src/de/joern/test/Test.java
@@ -1,78 +1,50 @@
 package de.joern.test;
 
-import de.joern.day1.Day1_1;
-import de.joern.day1.Day1_2;
-import de.joern.day10.Day10;
+import de.joern.Problems;
 import de.joern.ProblemSolver;
-import de.joern.day11.Day11;
-import de.joern.day12.Day12;
-import de.joern.day13.Day13;
-import de.joern.day14.Day14;
-import de.joern.day2.Day2_1;
-import de.joern.day2.Day2_2;
-import de.joern.day3.Day3_1;
-import de.joern.day3.Day3_2;
-import de.joern.day4.Day4_1;
-import de.joern.day4.Day4_2;
-import de.joern.day5.Day5;
-import de.joern.day6.Day6;
-import de.joern.day7.Day7;
-import de.joern.day8.Day8_1;
-import de.joern.day8.Day8_2;
-import de.joern.day9.Day9_1;
-import de.joern.day9.Day9_2;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.EnumMap;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Stream;
 
 public class Test {
-    public static void main(String[] args) throws IOException {
-        boolean result;
-        System.out.println("Day 1");
-        result = test(new Day1_1(), fromFile(1), 7);
-        result &= test(new Day1_2(), fromFile(1), 5);
-        System.out.println("Day 2");
-        result &= test(new Day2_1(), fromFile(2), 150);
-        result &= test(new Day2_2(), fromFile(2), 900);
-        System.out.println("Day 3");
-        result &= test(new Day3_1(), fromFile(3), 198);
-        result &= test(new Day3_2(), fromFile(3), 230);
-        System.out.println("Day 4");
-        result &= test(new Day4_1(), fromFile(4), 4512);
-        result &= test(new Day4_2(), fromFile(4), 1924);
-        System.out.println("Day 5");
-        result &= test(Day5.day5_1(), fromFile(5), 5);
-        result &= test(Day5.day5_2(), fromFile(5), 12);
-        System.out.println("Day 6");
-        result &= test(new Day6(80), fromFile(6), 5934);
-        result &= test(new Day6(256), fromFile(6), 26984457539L);
-        System.out.println("Day 7");
-        result &= test(Day7.day7_1(), fromFile(7), 37);
-        result &= test(Day7.day7_2(), fromFile(7), 168);
-        System.out.println("Day 8");
-        result &= test(new Day8_1(), fromFile(8), 26);
-        result &= test(new Day8_2(), fromFile(8), 61229);
-        System.out.println("Day 9");
-        result &= test(new Day9_1(), fromFile(9), 11);
-        result &= test(new Day9_2(), fromFile(9), 1134);
-        System.out.println("Day 10");
-        result &= test(Day10.day10_1(), fromFile(10), 26397);
-        result &= test(Day10.day10_2(), fromFile(10), 288957);
-        System.out.println("Day 11");
-        result &= test(Day11.day11_1(), fromFile(11), 1656);
-        result &= test(Day11.day11_2(), fromFile(11), 195);
-        System.out.println("Day 12");
-        result &= test(Day12.day12_1(), fromFile(12), 10);
-        result &= test(Day12.day12_2(), fromFile(12), 36);
-        System.out.println("Day 13");
-        result &= test(new Day13(), fromFile(13), 17);
-        System.out.println("Day 14");
-        result &= test(Day14.day14_1(), fromFile(14), 1588);
-        result &= test(Day14.day14_2(), fromFile(14), 2188189693529L);
+    private static final Map<Problems, List<Long>> EXPECTED_RESULTS = new EnumMap<>(Problems.class);
 
+    static {
+        EXPECTED_RESULTS.put(Problems.DAY1, List.of(7L, 5L));
+        EXPECTED_RESULTS.put(Problems.DAY2, List.of(150L, 900L));
+        EXPECTED_RESULTS.put(Problems.DAY3, List.of(198L, 230L));
+        EXPECTED_RESULTS.put(Problems.DAY4, List.of(4512L, 1924L));
+        EXPECTED_RESULTS.put(Problems.DAY5, List.of(5L, 12L));
+        EXPECTED_RESULTS.put(Problems.DAY6, List.of(5934L, 26984457539L));
+        EXPECTED_RESULTS.put(Problems.DAY7, List.of(37L, 168L));
+        EXPECTED_RESULTS.put(Problems.DAY8, List.of(26L, 61229L));
+        EXPECTED_RESULTS.put(Problems.DAY9, List.of(11L, 1134L));
+        EXPECTED_RESULTS.put(Problems.DAY10, List.of(26397L, 288957L));
+        EXPECTED_RESULTS.put(Problems.DAY11, List.of(1656L, 195L));
+        EXPECTED_RESULTS.put(Problems.DAY12, List.of(10L, 36L));
+        EXPECTED_RESULTS.put(Problems.DAY13, List.of(17L));
+        EXPECTED_RESULTS.put(Problems.DAY14, List.of(1588L, 2188189693529L));
+    }
+
+    public static void main(String[] args) throws IOException {
+        boolean result = true;
+
+        for (int i = 0; i < Problems.values().length; i++) {
+            System.out.printf("Day %d%n", i+1);
+            Problems problem = Problems.values()[i];
+            List<Long> expected = EXPECTED_RESULTS.get(problem);
+
+            List<ProblemSolver> solvers = problem.getSolvers();
+            for (int solve = 0; solve < solvers.size(); solve++) {
+                result &= test(solvers.get(solve), fromFile(i+1), expected.get(solve));
+            }
+        }
         System.out.println(result ? "all tests successful" : "test(s) failed");
     }
 
